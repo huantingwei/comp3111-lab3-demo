c534c6c321b477e2fad3331bf53850fb5f1e6665
huantingwei <ting02940@gmail.com>
2019-03-07 09:38:47 +0800

    add textfield1 and show its text on label1

--- src/main/java/lab3/ex2/Lab3Controller.java
+++ src/main/java/lab3/ex2/Lab3Controller.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Lab3Controller {
 
@@ -13,10 +14,13 @@ public class Lab3Controller {
 
     @FXML
     private Label label1;
+    
+    @FXML
+    private TextField textfield1;
 
     @FXML
     void buttonPressed(ActionEvent event) {
-
+    	label1.setText(textfield1.getText());
     }
 
 }
